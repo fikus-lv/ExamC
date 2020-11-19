4939d5cebbf51ad345ec9beec622f74d78202299
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-19 10:14:39 +0200

    8

--- ExamCP/Form1.Designer.cs
+++ ExamCP/Form1.Designer.cs
@@ -28,33 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ColumnHeader columnHeader2;
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(358, 12);
+            this.button1.Location = new System.Drawing.Point(408, 299);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(118, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Додати";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Location = new System.Drawing.Point(13, 13);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(340, 251);
+            this.listView1.Size = new System.Drawing.Size(602, 251);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Видалитти";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Title";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Description";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "StartDate";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "EndDate";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "State";
+            this.columnHeader6.Width = 88;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +118,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
     }
 }
 
---
+++ Realization/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace Realization
+{
+	[XmlRoot(ElementName = "Task")]
+	public class Task1
+	{
+		[XmlElement(ElementName = "Id")]
+		public string Id { get; set; }
+		[XmlElement(ElementName = "Title")]
+		public string Title { get; set; }
+		[XmlElement(ElementName = "Description")]
+		public string Description { get; set; }
+		[XmlElement(ElementName = "StartDate")]
+		public string StartDate { get; set; }
+		[XmlElement(ElementName = "EndDate")]
+		public string EndDate { get; set; }
+		[XmlElement(ElementName = "State")]
+		public string State { get; set; }
+	}
+
+	[XmlRoot(ElementName = "xml")]
+	public class Xml
+	{
+		[XmlElement(ElementName = "Task")]
+		public List<Task> Task { get; set; }
+	}
+
+}
